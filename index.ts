974ef66a7d9d51fc620465281660f5b72d41b614
ross-venado <venado800@gmail.com>
2020-05-08 00:13:40 -0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,43 @@
+import Server from './classes/server';
+import mongoose from 'mongoose';
+
+import cors from 'cors';
+
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
+
+import userRoutes from './routes/usuario';
+import postRoutes from './routes/post';
+
+const server = new Server();
+
+
+// Body parser
+server.app.use( bodyParser.urlencoded({ extended: true }));
+server.app.use( bodyParser.json() );
+
+
+// FileUpload
+server.app.use( fileUpload({ useTempFiles: true }) );
+
+server.app.use(cors({origin: true, credentials: true}));
+
+
+// Rutas de mi app
+server.app.use('/user', userRoutes );
+server.app.use('/posts', postRoutes );
+
+
+// Conectar DB
+mongoose.connect('mongodb://localhost:27017/fotosgram', 
+                { useNewUrlParser: true, useCreateIndex: true }, ( err ) => {
+
+   if ( err ) throw err;
+
+   console.log('Base de datos ONLINE');
+})
+
+// Levantar express
+server.start( () => {
+    console.log(`Servidor corriendo en puerto ${ server.port }`);
+});
---
+++ models/post.model.ts
@@ -0,0 +1,43 @@
+
+import { Schema, Document, model } from 'mongoose';
+
+const postSchema = new Schema({
+
+
+    created: {
+        type: Date
+    },
+    mensaje: {
+        type: String
+    },
+    imgs: [{
+        type: String
+    }],
+    coords: {
+        type: String   // -13.313123, 12.3123123
+    },    
+    bandera: {
+        type: String
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: [ true, 'Debe de existir una referencia a un usuario' ]
+    }
+});
+
+postSchema.pre<IPost>('save', function( next ) {
+    this.created = new Date();
+    next();
+});
+
+interface IPost extends Document {
+    created: Date;
+    mensaje: string;
+    img: string[];
+    coords: string;
+    bandera: string;
+    usuario: string;
+}
+
+export const Post = model<IPost>('Post', postSchema);
